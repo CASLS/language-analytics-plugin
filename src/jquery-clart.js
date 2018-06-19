8ae4cb23aa726e12faf02187fcde4c3ea8d5964e
carlb <burnstei@uoregon.edu>
2018-06-18 17:42:41 -0700

    CLART-1 #time 30m #comment -Added preliminary Apache License 2.0 license file to project. -Also added a preliminary header to jquery-clart.js  These are both subject to change.

--- src/jquery-clart.js
+++ src/jquery-clart.js
@@ -1,3 +1,27 @@
+/* ========================================================================
+ * CASLS Language Analytics & Reporting Tools: jquery-clart.js v1.0.1
+ * http://casls-stash.uoregon.edu/projects/CLART/repos/jquery-language-analytics-plugin
+ * ========================================================================
+ * Copyright 2018 Center for Applied Second Language Studies
+ * Author: Carl Burnstein
+ * Licensed under Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0.html)
+ * ======================================================================== 
+ * 
+ * Copyright 2018 Center for Applied Second Language Studies
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ * */
+
 (function ( $ ) {
 	var allComments = [['Phrases'],["This is a test comment"], ["This is another test comment"], ["This is a third test comment"]];
 	var commentWords = [];
